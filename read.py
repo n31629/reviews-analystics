471792bd972d9708664c0ed0786ae62bd5b8f9d2
Sean Tseng <n31629@gmail.com>
2020-01-02 00:54:53 +0800

    calc avg len

--- read.py
+++ read.py
@@ -7,4 +7,9 @@ with open('reviews.txt', 'r') as f:
         if count % 1000 == 0:
             print(len(data))
 
-print(data[0])
+print('檔案讀取完成，總共有', len(data), "筆資料")
+
+sum_len = 0
+for d in data:
+	sum_len += len(d) 
+print("平均每筆留言有", sum_len / len(data), "個字數")
